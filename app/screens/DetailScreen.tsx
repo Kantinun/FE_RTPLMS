621dc3a639fa9349c5f1b2cca50e30049e2bc73a
kantinun <s6201012630011@email.kmutnb.ac.th>
2022-12-22 17:15:51 +0700

    Decorate carousel card

--- app/screens/DetailScreen.tsx
+++ app/screens/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Dimensions,Button, StyleSheet, Text, View} from 'react-native';
+import {Dimensions,Button, StyleSheet, Text, View, Image} from 'react-native';
 
 import MainContainer from '../components/MainContainer';
 import RegularText from '../../assets/Texts/RegularText';
@@ -12,37 +12,42 @@ import Carousel from 'react-native-reanimated-carousel';
 const DetailScreen = ({route}: any) => {
   const [date, setDate] = useState(new Date());
   const width = Dimensions.get('window').width;
+  const myData = [{uri: 'https://picsum.photos/200'},{uri: 'https://picsum.photos/200/300'},{uri: 'https://picsum.photos/seed/picsum/200/300'}]
+
 
   return (
     <MainContainer>
       <RegularText>
         Department ID: {route.params.id}
       </RegularText>
-      <View>
+      <View style={{marginVertical: 10}}>
         <MyDateTimePicker date={date} setDate={(date) => {setDate(date)}} />
       </View>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, alignItems: 'center' }}>
             <Carousel
                 loop
                 width={width}
                 height={width / 2}
                 autoPlay={true}
-                data={[...new Array(6).keys()]}
+                autoPlayInterval={1500}
+                mode="parallax"
+                data={myData}
                 scrollAnimationDuration={1000}
                 onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-                    </View>
-                )}
+                renderItem={({ item }) => (
+                  <View style={{
+                      flex:1,
+                      alignItems: 'center',
+                  }}>
+                  <Image
+                      source={{
+                          uri: item.uri,
+                          width: '90%',
+                          height: '100%'
+                      }}
+                  ></Image>
+                  </View>
+              )}
             />
         </View>
     </MainContainer>
