7376039c3ef71124cd74ef1e0c66751075949e44
kantinun <s6201012630011@email.kmutnb.ac.th>
2022-12-22 16:23:21 +0700

    add carousel in details page

--- app/screens/DetailScreen.tsx
+++ app/screens/DetailScreen.tsx
@@ -1,17 +1,17 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Dimensions,Button, StyleSheet, Text, View} from 'react-native';
 
 import MainContainer from '../components/MainContainer';
 import RegularText from '../../assets/Texts/RegularText';
 import MyDateTimePicker from '../components/DateTimePicker';
-// import DatePicker from 'react-native-datepicker';
-import DatePicker from '@react-native-community/datetimepicker'
-// import DatePicker from 'react-native-modern-datepicker';
 import { getToday, getFormatedDate } from 'react-native-modern-datepicker';
 import moment from 'moment';
+import Carousel from 'react-native-reanimated-carousel';
+
 
 const DetailScreen = ({route}: any) => {
   const [date, setDate] = useState(new Date());
+  const width = Dimensions.get('window').width;
 
   return (
     <MainContainer>
@@ -21,6 +21,30 @@ const DetailScreen = ({route}: any) => {
       <View>
         <MyDateTimePicker date={date} setDate={(date) => {setDate(date)}} />
       </View>
+      <View style={{ flex: 1 }}>
+            <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
+        </View>
     </MainContainer>
   );
 };
