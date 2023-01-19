f361ee48bd43121429c39d48bf5a5f5afc617dd5
kantinun <s6201012630011@email.kmutnb.ac.th>
2023-01-19 13:49:30 +0700

    change modal component in add/del worker modal

--- app/components/Modal/add_del_worker_modal.tsx
+++ app/components/Modal/add_del_worker_modal.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Modal, Text, TouchableHighlight, View, TouchableWithoutFeedback, Button, StyleSheet, Pressable} from 'react-native';
+import { Text, TouchableHighlight, View, TouchableWithoutFeedback, Button, StyleSheet, Pressable} from 'react-native';
 import { Cell, Row, Rows, Table, TableWrapper } from 'react-native-table-component';
 import { SearchBar,CheckBox, Icon} from '@rneui/themed'
+import Modal from "react-native-modal";
 
 const Add_del_worker_modal = (props: any) => {
   const [data, setData] = useState(props.data)
@@ -20,53 +21,43 @@ const Add_del_worker_modal = (props: any) => {
         setData(newData)
   }
   return (
-    <View style={{marginTop: 22}}>
-      <Modal
-        transparent={true}
-        animationType="fade"
-        visible={props.visible}
-        onRequestClose={() => {
-          props.clickHandler(false);
-        }}
-      >
-        <TouchableWithoutFeedback onPress={() => props.clickHandler(false)}>
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.5)'}}>
-            <TouchableWithoutFeedback>
-              <View style={{backgroundColor: 'white', width: '100%'}}>
-                <SearchBar
-                  placeholder='Search Here...'
-                  containerStyle={{backgroundColor: 'white'}}
-                  inputContainerStyle={{backgroundColor: 'white', borderWidth: 1}}
-                  round={true}
-                  showCancel={true}
-                  lightTheme={true}
-                ></SearchBar>
-                <Table borderStyle={{borderWidth: 2, borderColor: '#eee'}}>
-                    <Row data={data.header} style={styles.head} textStyle={styles.text}></Row>
-                      {
-                        data.content.map((item)=>(
-                          <TableWrapper style={{ flexDirection: 'row'}}>
-                            <Cell data={<CheckBox
-                              center
-                              checked={item.isChecked}
-                              onPress={() => handleCheckboxClick(item.id)}
-                            />}> 
-                            </Cell>
-                            <Cell data={<Text style={{textAlign: 'center'}}>{item.name}</Text>}> </Cell>
-                            <Cell data={<Text style={{textAlign: 'center'}}>{item.performance}</Text>}> </Cell>
-                          </TableWrapper>
-                        ))
-                      }
-                </Table>
-                <View style={{width:'100%', flexDirection: 'row-reverse', padding: 5}}>
-                  <Button title='Confirm' onPress={()=>{console.log(data.content)}}></Button>
-                </View>
-              </View>
-            </TouchableWithoutFeedback>
+    <Modal
+      isVisible={props.visible}
+      onBackdropPress={() => {
+        props.clickHandler(false);
+      }}
+    >
+      <View style={{backgroundColor: 'white', width: '100%'}}>
+        <SearchBar
+            placeholder='Search Here...'
+            containerStyle={{backgroundColor: 'white'}}
+            inputContainerStyle={{backgroundColor: 'white', borderWidth: 1}}
+            round={true}
+            showCancel={true}
+            lightTheme={true}
+          ></SearchBar>
+          <Table borderStyle={{borderWidth: 2, borderColor: '#eee'}}>
+              <Row data={data.header} style={styles.head} textStyle={styles.text}></Row>
+                {
+                  data.content.map((item)=>(
+                    <TableWrapper style={{ flexDirection: 'row'}}>
+                      <Cell data={<CheckBox
+                        center
+                        checked={item.isChecked}
+                        onPress={() => handleCheckboxClick(item.id)}
+                      />}> 
+                      </Cell>
+                      <Cell data={<Text style={{textAlign: 'center'}}>{item.name}</Text>}> </Cell>
+                      <Cell data={<Text style={{textAlign: 'center'}}>{item.performance}</Text>}> </Cell>
+                    </TableWrapper>
+                  ))
+                }
+          </Table>
+          <View style={{width:'100%', flexDirection: 'row-reverse', padding: 5}}>
+            <Button title='Confirm' onPress={()=>{console.log(data.content)}}></Button>
           </View>
-        </TouchableWithoutFeedback>
-      </Modal>
-    </View>
+        </View>
+    </Modal>
   );
 };
 
