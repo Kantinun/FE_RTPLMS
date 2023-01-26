63ef360393995727e2ac989af42726360d593c35
kantinun <s6201012630011@email.kmutnb.ac.th>
2023-01-26 18:16:15 +0700

    add body of agendar component in tasks plan page

--- app/screens/Worker/TaskPlanScreen.tsx
+++ app/screens/Worker/TaskPlanScreen.tsx
@@ -1,79 +1,100 @@
 import React, { useState } from 'react';
-import { View, Text} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Agenda, DateData, AgendaEntry, AgendaSchedule } from 'react-native-calendars';
 import moment from 'moment';
 import { colors } from '../../config/colors';
 
+interface Props {}
+
 function TaskPlanScreen(props: unknown) {
-    const [date,setDate]=useState(moment().format('YYYY-MM-DD'))
-    return (
-        <View style={{flex:1, backgroundColor: 'green'}}>        
-            <Agenda
-            items={{
-                '2023-01-26': [{name: 'item 1 - any js object', height: 80}, {name: 'any js object', height: 80}, {name: 'any js object', height: 80}],
-                '2023-01-27': [{name: 'item 2 - any js object', height: 80}],
-                '2023-01-28': [],
-                '2023-01-29': [{name: 'item 3 - any js object', height: 80}, {name: 'any js object', height: 80}],
-                '2023-01-30': [{name: 'item 3 - any js object', height: 80}, {name: 'any js object', height: 80}],
-                '2023-01-31': [{name: 'item 3 - any js object', height: 80}, {name: 'any js object', height: 80}],
-                '2023-02-01': [{name: 'item 3 - any js object', height: 80}, {name: 'any js object', height: 80}],
-            }}
-            // selected={'2023-01-27'}
-            onDayPress={day => {
-                console.log('selected day', day);
-              }}
-            // renderKnob={()=>(<View style={{backgroundColor: colors.primaryLight, width: 75, height:10, borderRadius:40, opacity: 0.3, marginTop: 5}}></View>)}
-            pastScrollRange={3}
-            futureScrollRange={3}
-            showClosingKnob={true}
-            markedDates={{
-                '2023-01-26': {marked: true},
-                '2023-01-28': {marked: true},
-                '2023-01-27': {}
-            }}
-            renderEmptyDate = {() => {
-                return (
-                  <View style={{}}>
-                    <Text>This is empty date!</Text>
-                  </View>
-                );}
+    const [items, setItems] = useState<AgendaSchedule | undefined>();
+    const loadItems = (day: DateData) => {
+        const items = items || {};
+
+        setTimeout(() => {
+        for (let i = -15; i < 85; i++) {
+            const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+            const strTime = timeToString(time);
+
+            if (!items[strTime]) {
+            items[strTime] = [];
+
+            const numItems = Math.floor(Math.random() * 3 + 1);
+            for (let j = 0; j < numItems; j++) {
+                items[strTime].push({
+                name: 'Item for ' + strTime + ' #' + j,
+                height: Math.max(50, Math.floor(Math.random() * 150)),
+                day: strTime,
+                });
             }
-            refreshing={true}
-            theme={{
-                // backgroundColor: 'red',
-                calendarBackground: '#ffffff',
-                textSectionTitleColor: '#b6c1cd',
-                textSectionTitleDisabledColor: '#d9e1e8',
-                selectedDayBackgroundColor: colors.primaryDark,
-                selectedDayTextColor: 'white',
-                todayTextColor: colors.primaryLight,
-                todayBackgroundColor: 'white',
-                dayTextColor: '#2d4150',
-                textDisabledColor: '#d9e1e8',
-                dotColor: '#00adf5',
-                selectedDotColor: '#ffffff',
-                arrowColor: 'orange',
-                disabledArrowColor: '#d9e1e8',
-                monthTextColor: colors.primaryDark,
-                indicatorColor: 'blue',
-                textDayFontFamily: 'monospace',
-                textMonthFontFamily: 'monospace',
-                textDayHeaderFontFamily: 'monospace',
-                textDayFontWeight: '300',
-                textMonthFontWeight: 'bold',
-                textDayHeaderFontWeight: '300',
-                textDayFontSize: 16,
-                textMonthFontSize: 16,
-                textDayHeaderFontSize: 16,
-                // agendaDayTextColor: '#aaaa',
-                // agendaDayNumColor: '#aaaa',
-                agendaTodayColor: colors.primaryDark,
-                agendaKnobColor: '#aaaa',
-              }}
-            />
-            
+            }
+        }
+
+        const newItems: AgendaSchedule = {};
+        Object.keys(items).forEach((key) => {
+            newItems[key] = items[key];
+        });
+        setItems(newItems);
+        }, 1000);
+    };
+
+    const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+        const fontSize = isFirst ? 16 : 14;
+        const color = isFirst ? 'black' : '#43515c';
+
+        return (
+        <TouchableOpacity
+            style={[styles.item, { height: reservation.height }]}
+            onPress={() => Alert.alert(reservation.name)}
+        >
+            <Text style={{ fontSize, color }}>{reservation.name}</Text>
+        </TouchableOpacity>
+        );
+    };
+
+    const renderEmptyDate = () => {
+        return (
+        <View style={styles.emptyDate}>
+            <Text>This is empty date!</Text>
         </View>
-    )
+        );
+    };
+
+    const rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+        return r1.name !== r2.name;
+    };
+
+    const timeToString = (time: number) => {
+        const date = new Date(time);
+        return date.toISOString().split('T')[0];
+    }
+    return (
+        <Agenda
+        pastScrollRange={5}
+        futureScrollRange={5}
+        items={items}
+        loadItemsForMonth={loadItems}
+        renderItem={renderItem}
+        renderEmptyDate={renderEmptyDate}
+        rowHasChanged={rowHasChanged}
+        showClosingKnob={true}
+        />
+        );
 }
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17
+    },
+    emptyDate: {
+        height: 15,
+        flex: 1,
+        paddingTop: 30
+    }
+    });
 
 export default TaskPlanScreen;
