5a104aa28c14018c10e66319945b4e485f3c1c6e
korrawee <korrawee.somyos@gmail.com>
2023-03-11 11:50:03 +0700

    build: add websocket for update product/attendance to dashboard

--- app/screens/Manager/DashboardScreen.tsx
+++ app/screens/Manager/DashboardScreen.tsx
@@ -1,80 +1,110 @@
-import * as React from 'react';
-import {departmentCardData} from '../../../assets/typings';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
-import DepartmentCard from '../../components/DepartmentCard';
-import MainContainer from '../../components/MainContainer';
-import { get_departmentDetails } from '../../services/dashboard.service'
-import { SearchBar, Button} from '@rneui/themed'
-import { useState } from 'react';
-import { Appcontext } from '../../../AppContext';
-import moment from 'moment';
+import * as React from "react";
+import { departmentCardData } from "../../../assets/typings";
+import { FlatList, StyleSheet } from "react-native";
+import DepartmentCard from "../../components/DepartmentCard";
+import MainContainer from "../../components/MainContainer";
+import { get_departmentDetails } from "../../services/dashboard.service";
+import { SearchBar } from "@rneui/themed";
+import { useState } from "react";
+import { Appcontext } from "../../../AppContext";
+import moment from "moment";
+import { io } from "socket.io-client";
+import env from '../../config/env';
 
-const DashboardScreen = ({navigation}: any) => {
-  const {state} = React.useContext(Appcontext);
-  const [searchText, setSearchText] = useState('');
-  const [fetch_data, setFetch_data] =  useState<Array<departmentCardData>>([])
+const DashboardScreen = ({ navigation }: any) => {
+  const { state } = React.useContext(Appcontext);
+  const [searchText, setSearchText] = useState("");
+  const [fetch_data, setFetch_data] = useState<Array<departmentCardData>>([]);
   const [Data, setData] = React.useState(Array<departmentCardData>);
 
   const department_details = get_departmentDetails(state.data.id);
+  
+  // ===================
+  // Web Socket
+  // ===================
+  const websocket = io(`${env.API_BASE}:${env.API_PORT}`);
+  const updateSuccessProductTopic = `${state.data.id}-product`;
+  const updateAttendanceTopic = `${state.data.id}-attendance`;
+  websocket.on('connect', () => {
+    console.log('connected');
+  });
+  websocket.on(updateSuccessProductTopic, (d: Object)=>{
+    console.log(d);
+  })
+  websocket.on(updateAttendanceTopic, (d: Object)=>{
+    console.log(d);
+  })
+  // ===================
+  // ===================
 
   React.useEffect(() => {
-    department_details.then((res)=> {
-      res? setFetch_data(res): {department:[], shifts:[]}
-      res? setData(res) : {department:[], shifts:[]}
+    department_details.then((res) => {
+      res ? setFetch_data(res) : { department: [], shifts: [] };
+      res ? setData(res) : { department: [], shifts: [] };
     });
   }, []);
-  
+
   const handle_search = (text: string) => {
-    setSearchText(text)
-    let new_data = fetch_data.filter((row_data)=> row_data.department.title.toLowerCase().includes(text.toLowerCase()))
-    setData(new_data? new_data: fetch_data)
-    
-  }
-  
-  const renderDepartmentCard = ({item}: any) => {
+    setSearchText(text);
+    let new_data = fetch_data.filter((row_data) =>
+      row_data.department.title.toLowerCase().includes(text.toLowerCase())
+    );
+    setData(new_data ? new_data : fetch_data);
+  };
 
-    const current_shift = item.shift.filter((shift)=>{
-      let now = moment()
-      let shift_time = moment(`${shift.shiftDate} ${shift.shiftTime}`,"YYYY/MM/DD HH:mm:ss")
-      return (shift_time.isBefore(now) && shift_time.add(8, 'hours').isAfter(now))? shift: null
+  const renderDepartmentCard = ({ item }: any) => {
+    const current_shift = item.shift.filter((shift) => {
+      let now = moment();
+      let shift_time = moment(
+        `${shift.shiftDate} ${shift.shiftTime}`,
+        "YYYY/MM/DD HH:mm:ss"
+      );
+      return shift_time.isBefore(now) && shift_time.add(8, "hours").isAfter(now)
+        ? shift
+        : null;
       // Use pop() because fileter() will return Array but we need only the first object that match with condition then we use pop() to get it <3
-    })[0]
+    })[0];
 
-
-    return <DepartmentCard
-      detailScreenName={item.detailScreenName}
-      department={item.department}
-      shift={item.shift}
-      currentShift={current_shift}
-      testID="DepartmentCard"
-    />
+    return (
+      <DepartmentCard
+        detailScreenName={item.detailScreenName}
+        department={item.department}
+        shift={item.shift}
+        currentShift={current_shift}
+        testID="DepartmentCard"
+      />
+    );
   };
 
   return (
     <MainContainer>
       <SearchBar
-            placeholder='Search Here...'
-            containerStyle={{backgroundColor: 'white', borderRadius: 15}}
-            inputContainerStyle={{backgroundColor: '#eeee'}}
-            round={true}
-            lightTheme={true}
-            value={searchText}
-            onChangeText={(text)=>{
-              handle_search(text)
-            }}
-          ></SearchBar>
-      <FlatList style={ {width: '100%'}} data={Data} renderItem={renderDepartmentCard} />
+        placeholder="Search Here..."
+        containerStyle={{ backgroundColor: "white", borderRadius: 15 }}
+        inputContainerStyle={{ backgroundColor: "#eeee" }}
+        round={true}
+        lightTheme={true}
+        value={searchText}
+        onChangeText={(text) => {
+          handle_search(text);
+        }}
+      ></SearchBar>
+      <FlatList
+        style={{ width: "100%" }}
+        data={Data}
+        renderItem={renderDepartmentCard}
+      />
     </MainContainer>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    textAlign: 'left',
-    width: '100%',
+    textAlign: "left",
+    width: "100%",
   },
   card_list: {
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
 
